5af0ab5c5e4bcdef2b0e43f3e8b573420a239310
FaranikCo <110489919+FaranikCo@users.noreply.github.com>
2022-08-31 21:36:02 +0430

    Create LFB27_ReadSensors.ino

---
+++ examples/LFB27_ReadSensors.ino
@@ -0,0 +1,45 @@
+/*
+  LFB27_ReadSensors
+  Reads the output of IR sensors that connected to main board and write results on serial port.
+*/
+
+#define S1 10
+#define S2 11
+#define S3 12
+#define S4 A0
+#define S5 A1
+
+bool irSensors[5] = {0, 0, 0, 0, 0};
+int irPins[5] = {10, 11, 12, A0, A1};
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
+}
+
+void loop()
+{
+  readSensors();
+  delay(100);
+}
+
+
+void readSensors()
+{
+  Serial.print("irSensors: ");
+  for (int i = 0; i < 5; i++)
+  {
+    irSensors[i] = digitalRead(irPins[i]);
+    Serial.print(irSensors[i]);
+    if (i != 4)
+    {
+      Serial.print(",");
+    }
+  }
+  Serial.println();
+}
